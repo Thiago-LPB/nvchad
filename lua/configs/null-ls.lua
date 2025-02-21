ae9d1551cb57aa7d2e28788489cc5173397eed4a
Thiago-LPB <thiagoeumesm@gmail.com>
2025-02-21 17:27:50 -0300

    pythom_lspfix

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -2,7 +2,7 @@ local null_ls = require('null-ls')
 
 local opts = {
   sources = {
-    null_ls.builtins.diagnostics.black,
+    null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
   }
